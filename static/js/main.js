bd87845829c0548c9c326684988b1fce78ac6379
Anastasiya Kostyanitsyna <anastasiya.kost98@gmail.com>
2021-04-04 17:21:57 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,89 @@
+
+$(document).ready(
+	function() {
+	
+		let condition1 = (window.location.pathname == '/');
+
+		if (condition1) {
+ 			$("form#search-form").submit( catchSearchQuery );
+ 		} else {
+ 			preventPageReboot();
+ 			document.querySelector("#download-csv").addEventListener("click", function () {
+    			var html = document.querySelector("table").outerHTML;
+				export_table_to_csv(html, "table.csv");
+			});
+ 		};	
+})
+
+
+function catchSearchQuery(){
+	let query_values = $('form#search-form').serialize();
+	$.get("/api/places?" + query_values, function(data) {
+		$('#mainmap').attr('src', "/map_search");
+ 	});
+return false;
+}
+
+
+// Предотвращает перезагрузку страницы
+function preventPageReboot(){
+
+  $("#description-page").click( function(event) {
+    event.preventDefault();
+    window.open("/#row1", "_self");});
+    
+  $("#map-page").click( function(event) {
+    event.preventDefault();
+    window.open("/#row2", "_self");});
+    
+  $("#main-page").click( function(event) {
+    event.preventDefault();
+    window.open("/#", "_self");});
+
+}
+
+
+function download_csv(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+}
+
+
+function export_table_to_csv(html, filename) {
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+	
+    for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+		csv.push(row.join(";"));		
+	}
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+}
+
